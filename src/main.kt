a4e6bb3ca3694083352a99d1bc012d0a99b44a3d
Yazdani <yaz4dani@gmail.com>
2017-07-09 23:11:12 +0600

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+/**
+ * Created by Yazdani on 7/9/2017.
+ */
+
+fun main(args : Array<String>){
+
+
+    var a="Orange"
+
+    when(a){
+
+        "Red" ->
+                println("Value is Red")
+
+        "Yellow" ->
+                println("Value is Yellow")
+
+        "Orange" ->
+                println("Value is Orange")
+
+        else ->
+                println("Value is not match")
+    }
+
+   
+
+}
